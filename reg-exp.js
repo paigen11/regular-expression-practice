f762bb3e1c2290019960022fdbe6358097a3ee5b
paigen11 <paigen11@gmail.com>
2016-09-03 08:29:57 -0400

    Updated reg exp practice

--- reg-exp.js
+++ reg-exp.js
@@ -38,7 +38,13 @@ function caseConvert(str, conversionType){
  		str = str.join();
  	}
  	if(conversionType === "camelcase"){
-	 		convertedStr = str.toLowerCase().replace(/\s\w/g, function(x){return x[1].toUpperCase();});
+	 		convertedStr = str.toLowerCase().replace(/\s\w/g, 
+	 			function(x){
+	 				return x[1].toUpperCase();
+	 				// 1. x = ' i' ===> I
+	 				// 2. x = ' a' ===> A
+	 				// 3. x = ' g' ===> G
+	 			});
 	 	}else if(conversionType === "snakecase"){
 	 		convertedStr = str.replace(/\s/g, '-').toLowerCase();
 	 	}
@@ -47,3 +53,58 @@ function caseConvert(str, conversionType){
 
 console.log(caseConvert(["Hello my name is paige"], "snakecase"));
 console.log(caseConvert("HELLO my name is paige", "snakecase"));
+console.log(caseConvert(["Hello my name is paige"], "camelcase"));
+console.log(caseConvert("HELLO my name is paige", "camelcase"));
+
+function caseConvert(str, conversionType){
+	var stringArray = str.toLowerCase().split(' ');
+	var convertedStr = '';
+	var temp = [];
+	if(conversionType === 'camelcase'){
+		temp.push(stringArray[0]);
+		for(var i = 0; i < stringArray.length; i++){
+			temp.push(stringArray[i].replace(stringArray[i].charAt(0), stringArray[i].charAt(0).toUpperCase()));
+		convertedStr = temp.join('');
+			}
+		}else if(conversionType === 'snakecase'){
+			converterdStr = stringArray.join('-');
+		}else{
+			return 'Please enter a valid string and case option';
+		}
+		return convertedStr; 
+	}
+
+
+// OPTIONAL: If you want a second challenge together, create a function convertPhone that validates a str and 
+// converts it to a valid phone number. You MUST use .match and pass a regular expression. Whatever string is 
+// passed in, the function must format it into format: 555-555-5555. Extra numbers are assumed a mistake and 
+// dropped. If not enough numbers are provided in the string, return an error message. It is a good idea to 
+// make use of join, split, and other array functions as you are likely to hop back and forth.
+// BONUS: if not enough numbers are provided, check to see if there are any letters that can be converted to 
+// numbers via the number pad mapping.
+
+// Consider the possible inputs:
+// 555-555-5555
+// (555)555-5555
+// 555 555 5555
+// 555 555-555
+// 1-555-555-5555
+// (1)555-555-5555
+// 5555555555
+// 555-five-55-aebdgtxi;;3tt4greqr6seven
+// 5-trumprules-555-5-5-5-5-5-5
+// 2798504trumpforprez23587928579428795458792
+// 1-800-ninjas-are-cool
+// 1900-acdc4life.
+
+
+function convertPhone(phoneStr){
+    //Write some awesome code here!
+ 	var nums = phoneStr.match(/\d/g).join('').substring(0,10);
+ 	if(nums.length == 10){
+ 		// get 3, 3, and 4
+ 		return nums.replace(/(\d{3})(\d{3})(\d{4})/g, '($1) $2-$3');
+ 	}else{
+ 		return 'not enough characters';
+ 	}
+}
